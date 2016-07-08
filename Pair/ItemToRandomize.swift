699b051d58c2b7074c8f7104d09e40cc40c8b4cf
kpfister <kpfister@me.com>
2016-07-08 09:24:10 -0600

    Added Enitity
    Created project and added a ItemToRandomize entity. Minor changes to the orginization for Pair files.

---
+++ Pair/ItemToRandomize.swift
@@ -0,0 +1,17 @@
+//
+//  ItemToRandomize.swift
+//  Pair
+//
+//  Created by Karl Pfister on 7/8/16.
+//  Copyright © 2016 Karl Pfister. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class ItemToRandomize: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
---
+++ Pair/Pair/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pair
+//
+//  Created by Karl Pfister on 7/8/16.
+//  Copyright © 2016 Karl Pfister. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
